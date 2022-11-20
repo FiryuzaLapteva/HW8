8b861b9ae02fee583b3d03567a501e5387472a7d
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-11-20 19:16:07 +0300

    add main and controller modules

---
+++ controller.py
@@ -0,0 +1,11 @@
+import view
+
+def LaunchProject():
+    if view.Get_action() == 1:
+        print('do1')
+    elif view.Get_action() == 2:
+        print('do2')
+    elif view.Get_action() == 3:
+        print('do3')
+    elif view.Get_action() == 4:
+        print('do4')
