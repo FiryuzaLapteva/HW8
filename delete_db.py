e639f7803c550711202392cde049d08de6b91c51
Nadezhda <nadejda.gruzdeva@gmail.com>
2022-11-22 09:55:29 +0300

    delete_db edition

--- delete_db.py
+++ delete_db.py
@@ -8,20 +8,26 @@ def Delete_db():
     family = (input("Введите фамилию искомого ученика\n").title())# Первая буква автоматически заглавная
     with open("Pupils.csv","r", encoding='utf-8') as csvfile:
         newrows = []
-        is_student_in_db = False #маркер, проверяющий, если ли искомый ученик в базе данных
+        is_student_in_db = True #маркер, проверяющий, если ли искомый ученик в базе данных
         reader = csv.reader(csvfile) #открываем файл для чтения
         for row in reader:
             # записываем в список все строчки без family
             if family not in row:
                 newrows.append(row)
-
+            else:
+                is_student_in_db = False
+                
                 
     with open("Pupils.csv","w", encoding='utf-8', newline='',) as csvfile2:
             write=csv.writer(csvfile2, delimiter=',') # открываем файл для записи
             for line in newrows:
                 write.writerow(line)
 
-  
-            
+    if is_student_in_db:
+        print(f"Ученик {family} удален из базы.")
+    else:
+        print(f"Ученик {family} не был найден в базе.")
+                
+# delete_db()
 
         
